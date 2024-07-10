3d8af6032fa30af7d1e4a4b5e150846697b6936b
Yang Seok Yeong <seokyeong.yang@kakao.com>
2024-07-10 20:18:27 +0900

    improve(develop): UnknownException 사용성 개선

--- lib/core/exceptions/unknown_exception.dart
+++ lib/core/exceptions/unknown_exception.dart
@@ -1,8 +1,10 @@
 class UnknownException implements Exception {
-  UnknownException();
+  final String? point;
+
+  UnknownException({this.point});
 
   @override
   String toString() {
-    return 'UnknownException: 알 수 없는 에러가 발생했습니다.';
+    return 'UnknownException: $point - 알 수 없는 에러가 발생했습니다.';
   }
 }
