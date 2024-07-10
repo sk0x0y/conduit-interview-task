16b7ecdc604be150e33a04a9d6c33a85cd5a50f5
Yang Seok Yeong <seokyeong.yang@kakao.com>
2024-07-10 20:20:44 +0900

    refactor(develop): 메서드 개선과 모듈화 및 Exception Message 개선

--- lib/data/utils/expression_parser.dart
+++ lib/data/utils/expression_parser.dart
@@ -2,16 +2,18 @@ import 'package:conduit_interview_task/core/constants/operator.dart';
 import 'package:conduit_interview_task/core/exceptions/invalid_exception.dart';
 
 class ExpressionParser {
-  static parseLeftOperand(List<String> parsedInput) {
-    return double.parse(parsedInput[0]);
+  static double parseOperand(String operand) {
+    final parsedOperand = double.tryParse(operand);
+
+    if (parsedOperand == null) {
+      throw InvalidException('입력하신 피연산자($operand)가 유효하지 않습니다.');
+    }
+
+    return parsedOperand;
   }
 
   static String parseOperatorFromString(List<String> parsedInput) {
     return parsedInput[1];
-  }
-
-  static double parseRightOperand(List<String> parsedInput) {
-    return double.parse(parsedInput[2]);
   }
 
   static Operator parseOperatorToEnum(String operator) {
@@ -25,7 +27,7 @@ class ExpressionParser {
       case '/':
         return Operator.divide;
       default:
-        throw InvalidException('입력하신 연산자가 유효하지 않습니다.');
+        throw InvalidException('입력하신 연산자($operator)가 유효하지 않습니다.');
     }
   }
 }
